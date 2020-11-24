a6ac57e0513db6d6e4eb9f863fe57ea7a07b6c74
Drothschild89 <Drothschild89@gmail.com>
2020-11-24 14:20:30 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
 
+    @knowledge = []
+
+    def initialize(knowledge)
+        @knowledge = knowledge
+    end
+
+    def learn(string)
+        knowledge.push(string)
+    end
+    
 end
